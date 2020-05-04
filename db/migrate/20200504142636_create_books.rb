40dfe192fe132524e9d3c55a8fc1106f6f358dd1
sharshi <sharshib@gmail.com>
2020-05-04 10:30:40 -0400

    Add books model

--- db/migrate/20200504142636_create_books.rb
+++ db/migrate/20200504142636_create_books.rb
@@ -1,6 +1,13 @@
 class CreateBooks < ActiveRecord::Migration[5.2]
   def change
     create_table :books do |t|
+      t.string :title_en, null: false
+      t.string :title_he, null: false
+      t.string :title_he_tr, null: false
+      t.string :description, null: false
+      t.integer :section_id, not_null: true
+
+      t.index :section_id
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_04_142518) do
+ActiveRecord::Schema.define(version: 2020_05_04_142636) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", force: :cascade do |t|
+    t.string "title_en", null: false
+    t.string "title_he", null: false
+    t.string "title_he_tr", null: false
+    t.string "description", null: false
+    t.integer "section_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["section_id"], name: "index_books_on_section_id"
+  end
 
   create_table "sections", force: :cascade do |t|
     t.string "title_en", null: false
