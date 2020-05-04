1f5a067da7e9ef608a56684bda775344ad193ba6
sharshi <sharshib@gmail.com>
2020-05-04 10:27:18 -0400

    Add taanach sections model

---
+++ db/migrate/20200504142636_create_books.rb
@@ -0,0 +1,8 @@
+class CreateBooks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :books do |t|
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_04_142110) do
+ActiveRecord::Schema.define(version: 2020_05_04_142518) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "sections", force: :cascade do |t|
+    t.string "title_en", null: false
+    t.string "title_he", null: false
+    t.string "title_he_tr", null: false
+    t.string "description", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
